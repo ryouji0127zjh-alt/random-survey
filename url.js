fe0956e86f43c1e9758d98502c5e24b95849c1a2
ryouji0127zjh-alt <ryouji0127zjh@gmail.com>
2025-12-01 10:51:56 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,16 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/sZhjBacEMBkX3dpg9',
+	'https://forms.gle/i95C1PrvxUucyaE9A',
+	'https://forms.gle/b3fYq6dkepdUGU9H8',
+	'https://forms.gle/ga9kqhcdo11WfvA49',
+	'https://forms.gle/gTckhtcNcUwAGFjX8',
+	'https://forms.gle/chKi4V2qGw7hjW8A8',
+	'https://forms.gle/6r56caT5CPWMXjgd6',
+	'https://forms.gle/uThAubPdU1VPQKje8',
+	'https://forms.gle/gbsvh56q5kRWtFzcA',
+	'https://forms.gle/wSYWJXWsMCgucPJh9',
+	'https://forms.gle/4dBd36VErN7gY9N99',
+	'https://forms.gle/RvmXaYVLTQgg3yLp9',
 ];
